fd3dcd128c4bd1c8b2a22e62df41eede6478eab4
BenClosner <bdclosne@mtu.edu>
2015-06-16 05:27:47 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/modules.xml
    	MyApplication.iml

--- app/src/main/java/mattressmoney/myapplication/MainActivity.java
+++ app/src/main/java/mattressmoney/myapplication/MainActivity.java
@@ -1,17 +1,25 @@
 package mattressmoney.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "mattressmoney.myapplication.MESSAGE";
+    String msg = "Android : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "oncreate event");
     }
 
     @Override
@@ -34,5 +42,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
